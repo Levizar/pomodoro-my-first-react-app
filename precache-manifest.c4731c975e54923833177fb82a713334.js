abbfa131fc303996ec641b331c0a4260c124b80f
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2020-03-11 10:55:44 +0100

    Updates

---
+++ precache-manifest.c4731c975e54923833177fb82a713334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6d17e76fc01f502c26a741fb041fc9",
+    "url": "/pomodoro-my-first-react-app/index.html"
+  },
+  {
+    "revision": "7b71ce744696db24f44a",
+    "url": "/pomodoro-my-first-react-app/static/css/main.ef7dfe8a.chunk.css"
+  },
+  {
+    "revision": "597a93795713c91f7a4e",
+    "url": "/pomodoro-my-first-react-app/static/js/2.f305669c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodoro-my-first-react-app/static/js/2.f305669c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b71ce744696db24f44a",
+    "url": "/pomodoro-my-first-react-app/static/js/main.2da2a587.chunk.js"
+  },
+  {
+    "revision": "c5e4379c163ca6fab4ac",
+    "url": "/pomodoro-my-first-react-app/static/js/runtime-main.dbd44852.js"
+  }
+]);
